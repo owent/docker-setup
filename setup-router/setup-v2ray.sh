0c0e2648104d0f82e1f4ec776bf32b7bc6da7bba
owentou <owentou@tencent.com>
2020-01-13 09:40:10 +0800

    patch comment

--- setup-router/setup-v2ray.sh
+++ setup-router/setup-v2ray.sh
@@ -9,14 +9,8 @@
 #    https://www.netfilter.org/projects/nftables/manpage.html
 #    https://www.mankier.com/8/nft
 # IP http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
-
-cat delegated-apnic-latest | awk 'BEGIN{FS="|"}{if($2 == "CN" && $3 != "asn"){print $3 " " $4 " " $5}}'
-# ipv4 <start> <count> => ipv4 58.42.0.0 65536
-# ipv6 <prefix> <bits> => ipv6 2407:c380:: 32
-
-curl -L -o generate_dnsmasq_chinalist.sh https://github.com/cokebar/openwrt-scripts/raw/master/generate_dnsmasq_chinalist.sh
-chmod +x generate_dnsmasq_chinalist.sh
-sh generate_dnsmasq_chinalist.sh -d 114.114.114.114 -p 53 -s ss_spec_dst_bp -o /etc/dnsmasq.d/accelerated-domains.china.conf
+#    ipv4 <start> <count> => ipv4 58.42.0.0 65536
+#    ipv6 <prefix> <bits> => ipv6 2407:c380:: 32
 
 if [ -e "/opt/nftables/sbin" ]; then
     export PATH=/opt/nftables/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
