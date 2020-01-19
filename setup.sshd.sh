9ab9f8883f68d016a98dfca598641a8e963341c7
owentou <owentou@tencent.com>
2020-01-19 21:39:34 +0800

    add PermitUserEnvironment yes

--- setup.sshd.sh
+++ setup.sshd.sh
@@ -13,6 +13,7 @@ sed -i -r 's/^\s*#?\s*AllowTcpForwarding.*/AllowTcpForwarding yes/' /etc/ssh/ssh
 sed -i -r 's/^\s*#?\s*AllowAgentForwarding.*/AllowAgentForwarding yes/' /etc/ssh/sshd_config
 sed -i -r 's/^\s*#?\s*TCPKeepAlive.*/TCPKeepAlive yes/' /etc/ssh/sshd_config
 sed -i -r 's/^\s*#?\s*ListenAddress\s*(.*)/#ListenAddress \1/' /etc/ssh/sshd_config
+sed -i -r 's/^\s*#?\s*PermitUserEnvironment.*/PermitUserEnvironment yes/' /etc/ssh/sshd_config
 sed -i '$!N; /^\(.*\)\n\1$/!P; D' /etc/ssh/sshd_config
 
 # sed -i -r 's/^\s*#?\s*Port\s*22/Port 36000/' /etc/ssh/sshd_config
