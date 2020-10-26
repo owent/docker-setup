25f8530da3b048e298cdabdae8a89061552c5ff6
owent <admin@owent.net>
2020-10-26 15:24:43 +0800

    修订git及工具链构架脚本

--- setup-devtools/setup_git.sh
+++ setup-devtools/setup_git.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+# Require asciidoc and xmlto to build documents
+if [[ -z "$SETUP_INSTALL_PREFIX" ]]; then
+    SETUP_INSTALL_PREFIX=/opt
+fi
+
+mkdir -p $SETUP_INSTALL_PREFIX/bin ;
 mkdir -p $SETUP_INSTALL_PREFIX/git ;
-cd $SETUP_INSTALL_PREFIX/git ;
+cd $SETUP_INSTALL_PREFIX ;
 
 RE2C_VERSION=2.0.3 ;
 GIT_VERSION=2.28.0 ;
 GIT_LFS_VERSION=2.12.0 ;
 export PATH="$SETUP_INSTALL_PREFIX/bin:$PATH"
 
-wget https://github.com/skvadrik/re2c/releases/download/$RE2C_VERSION/re2c-$RE2C_VERSION.tar.xz;
+if [[ ! -e "re2c-$RE2C_VERSION.tar.xz" ]]; then
+    wget https://github.com/skvadrik/re2c/releases/download/$RE2C_VERSION/re2c-$RE2C_VERSION.tar.xz;
+    if [[ $? -ne 0 ]]; then
+        rm -f re2c-$RE2C_VERSION.tar.xz;
+    fi
+fi
 tar -axvf re2c-$RE2C_VERSION.tar.xz ;
 cd re2c-$RE2C_VERSION ;
 ./configure --prefix=$SETUP_INSTALL_PREFIX/re2c/$RE2C_VERSION --with-pic=yes;
@@ -26,7 +37,13 @@ done
 
 cd ..;
 
-wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.xz;
+if [[ ! -e "git-$GIT_VERSION.tar.xz" ]]; then
+    wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.xz ;
+    if [[ $? -ne 0 ]]; then
+        rm -f git-$GIT_VERSION.tar.xz;
+    fi
+fi
+
 tar -axvf git-$GIT_VERSION.tar.xz ;
 cd git-$GIT_VERSION;
 ./configure --prefix=$SETUP_INSTALL_PREFIX/git/$GIT_VERSION --with-curl --with-expat --with-openssl --with-libpcre2 --with-editor=vim ;
@@ -48,7 +65,13 @@ mkdir -p git-lfs;
 cd git-lfs;
 
 # git lfs
-wget https://github.com/git-lfs/git-lfs/releases/download/v$GIT_LFS_VERSION/git-lfs-linux-amd64-v$GIT_LFS_VERSION.tar.gz ;
+if [[ ! -e "git-lfs-linux-amd64-v$GIT_LFS_VERSION.tar.gz" ]]; then
+    wget https://github.com/git-lfs/git-lfs/releases/download/v$GIT_LFS_VERSION/git-lfs-linux-amd64-v$GIT_LFS_VERSION.tar.gz ;
+    if [[ $? -ne 0 ]]; then
+        rm -f git-lfs-linux-amd64-v$GIT_LFS_VERSION.tar.gz;
+    fi
+fi
+
 mkdir git-lfs-v$GIT_LFS_VERSION;
 cd git-lfs-v$GIT_LFS_VERSION ; 
 tar -axvf ../git-lfs-linux-amd64-v$GIT_LFS_VERSION.tar.gz ;
