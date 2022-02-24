c6c23c950462340ad50eeaf750513a14e8e8d085
owentou <owentou@tencent.com>
2022-02-24 20:57:40 +0800

    加个简单的syncthing配置
    Signed-off-by: owentou <owentou@tencent.com>

--- setup-router/bitwarden/create-bitwarden-pod.sh
+++ setup-router/bitwarden/create-bitwarden-pod.sh
@@ -1,72 +1,67 @@
 #!/bin/bash
 
 if [[ -e "/opt/nftables/sbin" ]]; then
-    export PATH=/opt/nftables/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+  export PATH=/opt/nftables/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
 else
-    export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+  export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
 fi
 
 export XDG_RUNTIME_DIR="/run/user/$UID"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
 
-RUN_USER=$(whoami);
+RUN_USER=$(whoami)
 # sudo loginctl enable-linger $RUN_USER
 
 if [[ "x$RUN_USER" == "x" ]] || [[ "x$RUN_USER" == "xroot" ]]; then
-    echo -e "\033[1;32m$0 can not run with\033[0;m \033[1;31m$RUN_USER\033[0;m" ;
-    exit 1;
+  echo -e "\033[1;32m$0 can not run with\033[0;m \033[1;31m$RUN_USER\033[0;m"
+  exit 1
 fi
 
 RUN_HOME=$(cat /etc/passwd | awk "BEGIN{FS=\":\"} \$1 == \"$RUN_USER\" { print \$6 }")
 
 if [[ "x$RUN_HOME" == "x" ]]; then
-    RUN_HOME="$HOME";
+  RUN_HOME="$HOME"
 fi
 
 if [[ "x$BITWARDEN_ETC_DIR" == "x" ]]; then
-    BITWARDEN_ETC_DIR="$RUN_HOME/bitwarden/etc";
+  BITWARDEN_ETC_DIR="$RUN_HOME/bitwarden/etc"
 fi
-mkdir -p "$BITWARDEN_ETC_DIR";
+mkdir -p "$BITWARDEN_ETC_DIR"
 
 if [[ "x$BITWARDEN_LOG_DIR" == "x" ]]; then
-    BITWARDEN_LOG_DIR="$RUN_HOME/bitwarden/log";
+  BITWARDEN_LOG_DIR="$RUN_HOME/bitwarden/log"
 fi
-mkdir -p "$BITWARDEN_LOG_DIR";
+mkdir -p "$BITWARDEN_LOG_DIR"
 
 if [[ "x$BITWARDEN_DATA_DIR" == "x" ]]; then
-    BITWARDEN_DATA_DIR="$RUN_HOME/bitwarden/data";
+  BITWARDEN_DATA_DIR="$RUN_HOME/bitwarden/data"
 fi
-mkdir -p "$BITWARDEN_DATA_DIR";
+mkdir -p "$BITWARDEN_DATA_DIR"
 
-if [[ "x$BITWARDEN_SSL_DIR" == "x" ]]; then
-    BITWARDEN_SSL_DIR="$RUN_HOME/bitwarden/ssl/";
-fi
-mkdir -p "$BITWARDEN_SSL_DIR";
-
-systemctl --user --all | grep -F container-bitwarden.service ;
+systemctl --user --all | grep -F container-bitwarden.service
 
 if [[ $? -eq 0 ]]; then
-    systemctl --user stop container-bitwarden ;
-    systemctl --user disable container-bitwarden ;
+  systemctl --user stop container-bitwarden
+  systemctl --user disable container-bitwarden
 fi
 
-podman container inspect bitwarden > /dev/null 2>&1
+podman container inspect bitwarden >/dev/null 2>&1
 
 if [[ $? -eq 0 ]]; then
-    podman stop bitwarden ;
-    podman rm -f bitwarden ;
+  podman stop bitwarden
+  podman rm -f bitwarden
 fi
 
 if [[ "x$BITWARDEN_UPDATE" != "x" ]]; then
-    podman image inspect docker.io/vaultwarden/server:latest > /dev/null 2>&1
-    if [[ $? -eq 0 ]]; then
-        podman image rm -f docker.io/vaultwarden/server:latest ;
-    fi
+  podman image inspect docker.io/vaultwarden/server:latest >/dev/null 2>&1
+  if [[ $? -eq 0 ]]; then
+    podman image rm -f docker.io/vaultwarden/server:latest
+  fi
 fi
 
-podman pull docker.io/vaultwarden/server:latest ;
+podman pull docker.io/vaultwarden/server:latest
 
-ADMIN_TOKEN=$(openssl rand -base64 48);
+ADMIN_TOKEN=$(openssl rand -base64 48)
 
 # -e SMTP_HOST=smtp.exmail.qq.com                                                   \
 # -e SMTP_FROM=admin@owent.net                                                      \
@@ -77,20 +72,19 @@ ADMIN_TOKEN=$(openssl rand -base64 48);
 
 # -e ROCKET_WORKERS=8
 
-podman run -d --name bitwarden  --security-opt label=disable                            \
-       -e SIGNUPS_ALLOWED=false -e WEBSOCKET_ENABLED=true                               \
-       -e ROCKET_ADDRESS=127.0.0.1 -e ROCKET_PORT=8381                                  \
-       -e WEBSOCKET_ADDRESS=127.0.0.1 -e WEBSOCKET_PORT=8382                            \
-       -e INVITATIONS_ALLOWED=false -e LOG_FILE=/logs/bitwarden.log                     \
-       -e ADMIN_TOKEN=$ADMIN_TOKEN                                                      \
-       --mount type=bind,source=$BITWARDEN_LOG_DIR/,target=/logs/                       \
-       -v $BITWARDEN_DATA_DIR/:/data/:Z                                                 \
-       --network=host docker.io/vaultwarden/server:latest
+podman run -d --name bitwarden --security-opt label=disable \
+  -e SIGNUPS_ALLOWED=false -e WEBSOCKET_ENABLED=true \
+  -e ROCKET_ADDRESS=127.0.0.1 -e ROCKET_PORT=8381 \
+  -e WEBSOCKET_ADDRESS=127.0.0.1 -e WEBSOCKET_PORT=8382 \
+  -e INVITATIONS_ALLOWED=false -e LOG_FILE=/logs/bitwarden.log \
+  -e ADMIN_TOKEN=$ADMIN_TOKEN \
+  --mount type=bind,source=$BITWARDEN_LOG_DIR/,target=/logs/ \
+  -v $BITWARDEN_DATA_DIR/:/data/:Z \
+  --network=host docker.io/vaultwarden/server:latest
 
 podman exec bitwarden ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
-podman generate systemd --name bitwarden | tee $BITWARDEN_ETC_DIR/container-bitwarden.service ;
+podman generate systemd --name bitwarden | tee $BITWARDEN_ETC_DIR/container-bitwarden.service
 
-systemctl --user enable $BITWARDEN_ETC_DIR/container-bitwarden.service ;
+systemctl --user enable $BITWARDEN_ETC_DIR/container-bitwarden.service
 systemctl --user restart container-bitwarden
-
---
+++ setup-router/syncthing/create-syncthing-discovery-pod.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+if [[ -e "/opt/nftables/sbin" ]]; then
+  export PATH=/opt/nftables/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+else
+  export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+fi
+
+export XDG_RUNTIME_DIR="/run/user/$UID"
+export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
+
+RUN_USER=$(whoami)
+# sudo loginctl enable-linger $RUN_USER
+
+if [[ "x$RUN_USER" == "x" ]] || [[ "x$RUN_USER" == "xroot" ]]; then
+  echo -e "\033[1;32m$0 can not run with\033[0;m \033[1;31m$RUN_USER\033[0;m"
+  exit 1
+fi
+
+RUN_HOME=$(cat /etc/passwd | awk "BEGIN{FS=\":\"} \$1 == \"$RUN_USER\" { print \$6 }")
+
+if [[ "x$RUN_HOME" == "x" ]]; then
+  RUN_HOME="$HOME"
+fi
+
+if [[ "x$SYNCTHING_DISCOVERY_LISTEN_PORT" == "x" ]]; then
+  SYNCTHING_DISCOVERY_LISTEN_PORT=8341
+fi
+if [[ "x$SYNCTHING_DISCOVERY_LISTEN_REPLICATION_PORT" == "x" ]]; then
+  SYNCTHING_DISCOVERY_LISTEN_REPLICATION_PORT=8351
+fi
+
+if [[ "x$SYNCTHING_ETC_DIR" == "x" ]]; then
+  SYNCTHING_ETC_DIR="$RUN_HOME/syncthing/etc"
+fi
+mkdir -p "$SYNCTHING_ETC_DIR"
+chmod 777 "$SYNCTHING_ETC_DIR"
+
+if [[ "x$SYNCTHING_DATA_DIR" == "x" ]]; then
+  SYNCTHING_DATA_DIR="$RUN_HOME/syncthing/data"
+fi
+mkdir -p "$SYNCTHING_DATA_DIR"
+chmod 777 "$SYNCTHING_DATA_DIR"
+
+if [[ "x$SYNCTHING_SSL_DIR" == "x" ]]; then
+  SYNCTHING_SSL_DIR="$RUN_HOME/syncthing/ssl/"
+fi
+mkdir -p "$SYNCTHING_SSL_DIR"
+chmod 777 "$SYNCTHING_SSL_DIR"
+
+if [[ "x$SYNCTHING_SSL_CERT" == "x" ]] && [[ -e "/home/website/ssl/fullchain.cer" ]]; then
+  SYNCTHING_SSL_CERT="/home/website/ssl/fullchain.cer"
+fi
+if [[ "x$SYNCTHING_SSL_CERT" != "x" ]] && [[ -e "$SYNCTHING_SSL_CERT" ]]; then
+  if [[ "x$SYNCTHING_UPDATE" != "x" ]] || [[ "x$SYNCTHING_UPDATE_SSL" != "x" ]] || [[ ! -e "$SYNCTHING_SSL_DIR/fullchain.cer" ]]; then
+    cp -f "$SYNCTHING_SSL_CERT" "$SYNCTHING_SSL_DIR/fullchain.cer"
+    chmod 666 "$SYNCTHING_SSL_DIR/fullchain.cer"
+  fi
+fi
+
+if [[ "x$SYNCTHING_SSL_KEY" == "x" ]] && [[ -e "/home/website/ssl/owent.net.key" ]]; then
+  SYNCTHING_SSL_KEY="/home/website/ssl/owent.net.key"
+fi
+if [[ "x$SYNCTHING_SSL_KEY" != "x" ]] && [[ -e "$SYNCTHING_SSL_KEY" ]]; then
+  if [[ "x$SYNCTHING_UPDATE" != "x" ]] || [[ "x$SYNCTHING_UPDATE_SSL" != "x" ]] || [[ ! -e "$SYNCTHING_SSL_DIR/st-discovery.x-ha.com.key" ]]; then
+    cp -f "$SYNCTHING_SSL_KEY" "$SYNCTHING_SSL_DIR/st-discovery.x-ha.com.key"
+    chmod 666 "$SYNCTHING_SSL_DIR/st-discovery.x-ha.com.key"
+  fi
+fi
+
+systemctl --user --all | grep -F container-syncthing-discovery.service
+
+if [[ $? -eq 0 ]]; then
+  systemctl --user stop container-syncthing-discovery
+  systemctl --user disable container-syncthing-discovery
+fi
+
+podman container inspect syncthing-discovery >/dev/null 2>&1
+
+if [[ $? -eq 0 ]]; then
+  podman stop syncthing-discovery
+  podman rm -f syncthing-discovery
+fi
+
+if [[ "x$SYNCTHING_UPDATE" != "x" ]]; then
+  podman image inspect docker.io/syncthing/discosrv:latest >/dev/null 2>&1
+  if [[ $? -eq 0 ]]; then
+    podman image rm -f docker.io/syncthing/discosrv:latest
+  fi
+fi
+
+podman pull docker.io/syncthing/discosrv:latest
+
+# Use these options if the discovery is not under a reserve proxy and remove -http
+#   -cert=/syncthing/ssl/fullchain.cer \
+#   -key=/syncthing/ssl/st-discovery.x-ha.com.key \
+podman run -d --name syncthing-discovery --security-opt label=disable \
+  --mount type=bind,source=$SYNCTHING_SSL_DIR,target=/syncthing/ssl/ \
+  --mount type=bind,source=$SYNCTHING_DATA_DIR,target=/syncthing/data/ \
+  -p $SYNCTHING_DISCOVERY_LISTEN_PORT:8443/tcp \
+  docker.io/syncthing/discosrv:latest \
+  -cert=/syncthing/ssl/fullchain.cer \
+  -key=/syncthing/ssl/st-discovery.x-ha.com.key \
+  -http \
+  -db-dir=/syncthing/data
+
+podman exec syncthing-discovery ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+podman generate systemd --name syncthing-discovery | tee $SYNCTHING_ETC_DIR/container-syncthing-discovery.service
+
+systemctl --user enable $SYNCTHING_ETC_DIR/container-syncthing-discovery.service
+systemctl --user restart container-syncthing-discovery
