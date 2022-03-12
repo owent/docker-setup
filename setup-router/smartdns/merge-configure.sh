379ca60fb36f398d4b66f314d956854b1056456c
owent <admin@owent.net>
2022-03-12 17:10:30 +0800

    Add custom rule for smartdns
    Signed-off-by: owent <admin@owent.net>

--- setup-router/smartdns/merge-configure.sh
+++ setup-router/smartdns/merge-configure.sh
@@ -10,6 +10,10 @@ bind-tcp [::]:$SMARTDNS_DNS_PORT" >"$SMARTDNS_ETC_DIR/smartdns.conf"
 
 cat "$SCRIPT_DIR/smartdns.origin.conf" >>"$SMARTDNS_ETC_DIR/smartdns.conf"
 
+for CONFIGURE_FILE in "$SCRIPT_DIR/"*.router.smartdns.conf; do
+  cat "$CONFIGURE_FILE" >>"$SMARTDNS_ETC_DIR/smartdns.conf"
+done
+
 if [[ "x$SMARTDNS_APPEND_CONFIGURE" != "x" ]]; then
   echo "$SMARTDNS_APPEND_CONFIGURE" >>"$SMARTDNS_ETC_DIR/smartdns.conf"
 fi
