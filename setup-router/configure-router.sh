8ae046ebe751a50b3bfa9fec59d96a13807dd242
owent <admin@owent.net>
2022-03-14 11:31:58 +0800

    增加本地设置
    Signed-off-by: owent <admin@owent.net>

--- setup-router/configure-router.sh
+++ setup-router/configure-router.sh
@@ -10,7 +10,7 @@ export GEOIP_GEOSITE_ETC_DIR=$ROUTER_HOME/etc/v2ray
 export SMARTDNS_DNS_PORT=53
 export SMARTDNS_ETC_DIR=$ROUTER_HOME/etc/smartdns
 export SMARTDNS_LOG_DIR=/data/logs/smartdns
-export SMARTDNS_APPEND_CONFIGURE="address /home.shkits.com/172.18.1.10"
+export SMARTDNS_APPEND_CONFIGURE=""
 
 # Use radvd and ndp and disable NAT6
 export NAT_SETUP_SKIP_IPV6=1
