05e28c748eb60e689bfe5206a749d66f7a6d2ce1
owent <admin@owent.net>
2023-08-18 11:28:45 +0800

    Update debian source replacement

--- replace-source.sh
+++ replace-source.sh
@@ -37,16 +37,19 @@ elif [ "x$DISTRIBUTE_NAME" == "xubuntu" ]; then
 
   apt update -y
 elif [ "x$DISTRIBUTE_NAME" == "xdebian" ] || [[ "${DISTRIBUTE_LIKE_NAMES[@]}" =~ "debian" ]]; then
-  if [ ! -e "/etc/apt/sources.list.bak" ]; then
-    cp /etc/apt/sources.list /etc/apt/sources.list.bak
-  fi
-
   if [ -e "/etc/apt/sources.list" ]; then
+    if [ ! -e "/etc/apt/sources.list.bak" ]; then
+      cp /etc/apt/sources.list /etc/apt/sources.list.bak
+    fi
+
     sed -i -r 's;https?://.*/(debian-security/?);http://mirrors.tencent.com/\1;g' /etc/apt/sources.list
     sed -i -r 's;https?://.*/(debian/?);http://mirrors.tencent.com/\1;g' /etc/apt/sources.list
   fi
 
   if [ -e "/etc/apt/sources.list.d/debian.sources" ]; then
+    if [ ! -e "/etc/apt/sources.list.d/debian.sources.bak" ]; then
+      cp /etc/apt/sources.list.d/debian.sources /etc/apt/sources.list.d/debian.sources.bak
+    fi
     sed -i -r 's;https?://.*/(debian-security/?);http://mirrors.tencent.com/\1;g' /etc/apt/sources.list.d/debian.sources
     sed -i -r 's;https?://.*/(debian/?);http://mirrors.tencent.com/\1;g' /etc/apt/sources.list.d/debian.sources
   fi
