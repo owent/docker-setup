d22f9cc1800024e18363f14ad4da5a2330ae7906
owent <admin@owent.net>
2024-04-01 01:26:24 +0800

    vbox配置测试

--- setup-router/sing-box/create-client-pod.sh
+++ setup-router/sing-box/create-client-pod.sh
@@ -40,8 +40,8 @@ fi
 podman run -d --name vbox-client --cap-add=NET_ADMIN --cap-add=NET_BIND_SERVICE \
   --network=host --security-opt label=disable \
   --mount type=bind,source=$VBOX_ETC_DIR,target=/etc/vbox/,ro=true \
-  --mount type=bind,source=$VBOX_DATA_DIR/v2ray,target=/var/lib/vbox \
-  --mount type=bind,source=$ROUTER_LOG_ROOT_DIR/v2ray,target=/data/logs/vbox \
+  --mount type=bind,source=$VBOX_DATA_DIR,target=/var/lib/vbox \
+  --mount type=bind,source=$ROUTER_LOG_ROOT_DIR/vbox,target=/var/log/vbox \
   --mount type=bind,source=$ACMESH_SSL_DIR,target=/data/ssl,ro=true \
   docker.io/owt5008137/vbox:latest -D /var/lib/vbox -C /etc/vbox/ run
 
