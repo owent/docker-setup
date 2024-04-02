754d04cfa45355f5d2aecaf64bdfd9084ed431da
owent <owentou@tencent.com>
2024-04-02 22:44:46 +0800

    caddyserver

--- setup-router/caddy/build.Dockerfile
+++ setup-router/caddy/build.Dockerfile
@@ -1,8 +1,14 @@
-FROM caddy:builder AS builder
+FROM docker.io/caddy:builder AS builder
 
 RUN xcaddy build --with github.com/caddy-dns/cloudflare --with github.com/caddy-dns/dnspod
 
-FROM caddy:latest
+FROM docker.io/caddy:latest
 
 COPY --from=builder /usr/bin/caddy /usr/bin/caddy
-# COPY Caddyfile /etc/caddy/Caddyfile
+COPY Caddyfile /etc/caddy/Caddyfile
+
+EXPOSE 80
+EXPOSE 443
+
+# 容器启动命令
+CMD ["caddy", "run", "--config", "/etc/caddy/Caddyfile", "--adapter", "caddyfile"]
---
+++ setup-router/caddy/create-router-pod.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+source "$(dirname "$SCRIPT_DIR")/configure-router.sh"
+
+# Require net.ipv4.ip_unprivileged_port_start=80 in /etc/sysctl.d/*.conf
+# See https://github.com/containers/podman/blob/master/rootless.md
+
+if [[ "x$CADDY_UPDATE" != "x" ]] || [[ "x$ROUTER_IMAGE_UPDATE" != "x" ]]; then
+  podman pull docker.io/caddy:builder
+  podman pull docker.io/caddy:latest
+  if [[ $? -ne 0 ]]; then
+    echo "Pull docker.io/caddy:builder and docker.io/caddy:latest failed"
+    exit 1
+  fi
+fi
+
+if [[ ! -e "Caddyfile" ]]; then
+  cp -f sample.Caddyfile Caddyfile
+fi
+
+podman build --layers --force-rm --tag local-caddy -f build.Dockerfile .
+
+if [[ "root" == "$(id -un)" ]]; then
+  SYSTEMD_SERVICE_DIR=/lib/systemd/system
+else
+  SYSTEMD_SERVICE_DIR="$HOME/.config/systemd/user"
+  mkdir -p "$SYSTEMD_SERVICE_DIR"
+fi
+
+if [[ "$SYSTEMD_SERVICE_DIR" == "/lib/systemd/system" ]]; then
+  systemctl --all | grep -F router-caddy.service >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    systemctl stop router-caddy.service
+    systemctl disable router-caddy.service
+  fi
+else
+  export XDG_RUNTIME_DIR="/run/user/$UID"
+  export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
+
+  # Maybe need run from host: loginctl enable-linger tools
+  # see https://wiki.archlinux.org/index.php/Systemd/User
+  # sudo loginctl enable-linger $RUN_USER
+  systemctl --user --all | grep -F router-caddy.service >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    systemctl --user stop router-caddy.service
+    systemctl --user disable router-caddy.service
+  fi
+fi
+
+podman container inspect router-caddy >/dev/null 2>&1
+
+if [[ $? -eq 0 ]]; then
+  podman stop router-caddy
+  podman rm -f router-caddy
+fi
+
+if [[ "x$CADDY_UPDATE" != "x" ]] || [[ "x$ROUTER_IMAGE_UPDATE" != "x" ]]; then
+  podman image prune -a -f --filter "until=240h"
+fi
+
+mkdir -p "$ROUTER_LOG_ROOT_DIR/caddy"
+if [[ "x$ARIA2_DATA_ROOT" == "x" ]]; then
+  if [[ ! -z "$SAMBA_DATA_DIR" ]]; then
+    ARIA2_DATA_ROOT="$SAMBA_DATA_DIR/download"
+  elif [[ ! -z "$ROUTER_DATA_ROOT_DIR" ]]; then
+    ARIA2_DATA_ROOT="$ROUTER_DATA_ROOT_DIR/aria2/download"
+  else
+    ARIA2_DATA_ROOT="$HOME/aria2/download"
+  fi
+fi
+mkdir -p "$ARIA2_DATA_ROOT"
+
+CADDY_MOUNT_DIRS=("--mount" "type=bind,source=$ARIA2_DATA_ROOT,target=/data/website/html/downloads")
+
+if [[ "x$NEXTCLOUD_REVERSE_ROOT_DIR" != "x" ]]; then
+  CADDY_MOUNT_DIRS=(${CADDY_MOUNT_DIRS[@]}
+    "--mount" "type=bind,source=$NEXTCLOUD_REVERSE_ROOT_DIR/nextcloud,target=/data/website/html/nextcloud"
+    "--mount" "type=bind,source=$NEXTCLOUD_APPS_DIR,target=/data/website/html/nextcloud/custom_apps")
+fi
+
+podman run -d --name router-caddy --security-opt label=disable \
+  --mount type=bind,source=$ACMESH_SSL_DIR,target=/data/ssl,ro=true \
+  --mount type=bind,source=$ROUTER_LOG_ROOT_DIR/caddy,target=/var/log/caddy \
+  ${CADDY_MOUNT_DIRS[@]} \
+  --network=host local-caddy
+
+podman generate systemd router-caddy | tee -p "$SYSTEMD_SERVICE_DIR/router-caddy.service"
+podman container stop router-caddy
+
+if [[ "$SYSTEMD_SERVICE_DIR" == "/lib/systemd/system" ]]; then
+  systemctl enable router-caddy.service
+  systemctl daemon-reload
+  systemctl start router-caddy.service
+else
+  systemctl --user enable "$SYSTEMD_SERVICE_DIR/router-caddy.service"
+  systemctl --user daemon-reload
+  systemctl --user start router-caddy.service
+fi
