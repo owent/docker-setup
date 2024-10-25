3ed8d9e106ee83d61e508b27993851238c5f98f6
owent <owentou@tencent.com>
2024-10-25 15:07:27 +0800

    Update envs

--- setup-router/sing-box/setup-server.sh
+++ setup-router/sing-box/setup-server.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 if [[ "$(whoami)" == "tools" ]]; then
-  env VBOX_ETC_DIR=/data/vbox-server/etc VBOX_LOG_DIR=/data/vbox-server/log VBOX_SSL_DIR=/home/website/ssl $PWD/create-server-pod.sh
+  env VBOX_ETC_DIR=/data/vbox-server/etc VBOX_LOG_DIR=/data/vbox-server/log VBOX_SSL_DIR=/home/website/ssl VBOX_UPDATE=$VBOX_UPDATE ROUTER_IMAGE_UPDATE=$ROUTER_IMAGE_UPDATE $PWD/create-server-pod.sh
 else
-  su - tools -c "env VBOX_ETC_DIR=/data/vbox-server/etc VBOX_LOG_DIR=/data/vbox-server/log VBOX_SSL_DIR=/home/website/ssl $PWD/create-server-pod.sh"
+  su - tools -c "env VBOX_ETC_DIR=/data/vbox-server/etc VBOX_LOG_DIR=/data/vbox-server/log VBOX_SSL_DIR=/home/website/ssl VBOX_UPDATE=$VBOX_UPDATE ROUTER_IMAGE_UPDATE=$ROUTER_IMAGE_UPDATE $PWD/create-server-pod.sh"
 fi
